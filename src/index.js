6ea09fcd6748c0b1a328cc38b1754aca2d782f06
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-07-10 23:22:00 +0100

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-export const placeHolder = () => {
-  return 0;
+const morseCodeWordGap = '   ';
+const morseCodeLetterGap = ' ';
+
+export const decodeMorse = morseCode => {
+    let decodedMorseString = '';
+    const words = morseCode.trim().split(morseCodeWordGap);
+
+    words.forEach(word => {
+        const letters = word.split(morseCodeLetterGap);
+        letters.forEach(letter => {
+            decodedMorseString += MORSE_CODE[letter].toUpperCase();
+        });
+        words.indexOf(word) !== words.length - 1 ? decodedMorseString += ' ' : decodedMorseString;
+    })
+
+    return decodedMorseString;
+};
+
+const MORSE_CODE = {
+    "-----":"0",
+    ".----":"1",
+    "..---":"2",
+    "...--":"3",
+    "....-":"4",
+    ".....":"5",
+    "-....":"6",
+    "--...":"7",
+    "---..":"8",
+    "----.":"9",
+    ".-":"a",
+    "-...":"b",
+    "-.-.":"c",
+    "-..":"d",
+    ".":"e",
+    "..-.":"f",
+    "--.":"g",
+    "....":"h",
+    "..":"i",
+    ".---":"j",
+    "-.-":"k",
+    ".-..":"l",
+    "--":"m",
+    "-.":"n",
+    "---":"o",
+    ".--.":"p",
+    "--.-":"q",
+    ".-.":"r",
+    "...":"s",
+    "-":"t",
+    "..-":"u",
+    "...-":"v",
+    ".--":"w",
+    "-..-":"x",
+    "-.--":"y",
+    "--..":"z",
+    "/":" "
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,12 @@
-import { placeHolder } from './index.js';
+import { decodeMorse } from './index.js';
 import chai from 'chai';
 
 const { expect, should, assert } = chai;
 
-describe('some test', () => {
-  it('SHOULD do something', () => {
-      expect(placeHolder()).to.be.eql(0);
-  });
+describe('GIVEN decodeMorse function', () => {
+    describe(`AND the input is '.... . -.--   .--- ..- -.. .'`, () => {
+        it(`SHOULD return 'HEY JUDE'`, () => {
+            expect(decodeMorse('.... . -.--   .--- ..- -.. .')).to.eql('HEY JUDE');
+        });
+    });
 });
